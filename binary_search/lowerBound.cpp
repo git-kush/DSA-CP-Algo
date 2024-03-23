0ae3738424935cb3b9ea18df13783a0893f56764
git-kush <kushagrasingh734@gmail.com>
2024-03-23 15:26:29 +0530

    lpB-upB

---
+++ binary_search/lowerBound.cpp
@@ -0,0 +1,25 @@
+int lowerBound(vector<int> arr, int n, int x) {
+	// Write your code here   
+	// For a sorted array 'arr', 'lower_bound' of a number 'x' is defined as the smallest index 'idx' 
+	// such that the value 'arr[idx]' >= 'x'
+	int l(0), r(n-1),ans(n);
+
+	while(l  <=  r)
+	{	
+		int mid = (l+r)/2;
+
+		if(arr[mid] >= x)
+		{
+			r = mid-1;
+			ans = mid;
+		}
+
+		else
+		{
+			l = mid+1;
+		}
+
+	}
+
+	return ans;
+}
