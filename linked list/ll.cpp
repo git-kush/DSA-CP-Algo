22070c1278e3ac48626fd6412333db662483351b
git-kush <kushagrasingh734@gmail.com>
2024-03-26 15:20:56 +0530

    ll,,

--- linked list/ll.cpp
+++ linked list/ll.cpp
@@ -17,6 +17,7 @@ public:
 };
 
 Node* convertArr2LL(vector<int> arr){
+    Node* head = new Node(arr[0]);
     Node* temp = head;
     for(int i=1; i<arr.size(); i++){
         temp->next = new Node(arr[i]);
@@ -33,6 +34,130 @@ int lengthOFLL(Node* head){
         temp = temp->next;
     }
   return count;
+}
+
+Node * deleteHead(Node *head) {
+    if(head == NULL) return head;
+    Node* temp = head;
+    head = head->next;
+    delete temp;
+    return head;
+}
+
+Node *deleteLast(Node *list){
+    if(list == NULL || list->next ==nullptr) return NULL;
+    
+    Node* temp = list;
+    while(temp->next->next != nullptr){
+        temp = temp->next;
+    }
+    delete temp->next;
+    temp->next = nullptr;
+    return list;
+}
+
+// Node *deleteNode(Node *head, int pos)                   //to delete a node (my code)
+// {
+
+//   //empty LL
+//   if(head == NULL) return head;
+
+//   //removing head, i.e, pos = 0
+//   if(pos==0){
+//     Node* temp = head;
+//     head = head->next;
+//     delete temp;
+//     return head;
+//   }
+
+//   //normal case but we also have to consider that pos(zero based) < lengthOfLL
+//   int count(0);
+//   Node* temp = head;
+//   while(temp != NULL && count < pos-1){
+//     temp = temp->next;
+//     count++;
+//   }
+
+//   if(count == pos-1 && temp !=NULL && temp->next != NULL){
+//     Node* delNode = temp->next;
+//     temp->next = temp->next->next;
+//     delete delNode;
+//   }
+
+//   return head;
+
+// }
+
+Node *deleteNode(Node *head, int pos)            //cleaner
+{ 
+
+  //empty LL
+  if(head == NULL) return head;
+
+  //removing head, i.e, pos = 0
+  if(pos==0){
+    Node* temp = head;
+    head = head->next;
+    delete temp;
+    return head;
+  }
+
+  //normal case but we also have to consider that pos(zero based)+1 <= lengthOfLL
+  int count(0);
+  Node* temp = head;
+  Node* prev = NULL;
+  while(temp !=NULL){
+    
+    if(count == pos){
+      prev->next = prev->next->next;
+      free(temp);
+      break;
+    }
+    count++;
+    prev = temp;
+    temp = temp->next;
+  }
+
+  return head;
+
+}
+
+
+Node * insert(Node * head, int n, int pos, int val) {
+    // Write your code here
+    if(head == NULL) return new Node(val);
+    
+    if(pos == 0){
+        Node* temp = head;
+        Node* newNODE = new Node(val);
+        newNODE->next = temp;
+        return newNODE;
+    }
+
+    if(pos == n){
+        Node* newNODE = new Node(val);
+        Node* temp = head;
+        while( temp->next != NULL){
+            temp = temp->next;
+        }
+        temp->next = newNODE;
+        return head;
+    }
+    
+    Node* temp = head;
+    Node* prev = NULL;
+    int count(0);
+
+    while(pos--){
+        prev = temp;
+        temp = temp->next;
+    }
+    
+    Node* newNODE = new Node(val);
+    prev->next = newNODE;
+    newNODE->next = temp;
+    return head;
+
 }
 
 int main(){
